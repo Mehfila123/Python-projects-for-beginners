2171709d398962cf25cb45d940633563d98a4060
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-19 17:40:51 +0530

    Update and rename learned.py to Basic-addition.py

---
+++ Basic-addition.py
@@ -0,0 +1,10 @@
+# Addition  
+# Seeking input
+
+print ("Write two numbers which are integers")
+number_1 = int(input ("enter your first number :"))
+number_2 = int(input ("enter yuur second number :"))
+sum = number_1 + number_2
+print("The sum of your numbers are:" , sum)
+
+
