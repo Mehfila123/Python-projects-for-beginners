7048347cc0dad10589ff69d56b0e7db44a551d72
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-19 17:41:38 +0530

    Create Basics-subtaction.py

---
+++ Basics-subtaction.py
@@ -0,0 +1,25 @@
+
+diff = number_1 - number_2
+
+
+prod = number_1 *  number_2
+
+
+
+
+print("The difference of your numbers are:" , diff)
+
+print("The product of your numbers are:" ,prod)
+
+#BODMAS
+
+A , B = 2 , 4
+
+C = 8
+
+
+ans = A + B * C
+
+print("If A= 2, B=4 C=8 then, ")
+
+print("A+B*C  is : " , ans)
