253b992466137faa523c7caad9633ee16148105b
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-19 17:46:05 +0530

    Update Basics-subtaction.py

--- Basics.py/Basics-subtaction.py
+++ Basics.py/Basics-subtaction.py
@@ -1,25 +1,9 @@
+# Subraction 
+# Seeking input
 
+print ("Write two numbers which are integers")
+number_1 = int(input ("enter your first number :"))
+number_2 = int(input ("enter yuur second number :"))
 diff = number_1 - number_2
-
-
-prod = number_1 *  number_2
-
-
-
-
 print("The difference of your numbers are:" , diff)
 
-print("The product of your numbers are:" ,prod)
-
-#BODMAS
-
-A , B = 2 , 4
-
-C = 8
-
-
-ans = A + B * C
-
-print("If A= 2, B=4 C=8 then, ")
-
-print("A+B*C  is : " , ans)
