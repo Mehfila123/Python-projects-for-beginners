68e996842d01fc93e8493eb4464849fa5d7e3640
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-19 17:48:22 +0530

    Update Product.py

--- Basics.py/Product.py
+++ Basics.py/Product.py
@@ -1,5 +1,9 @@
+# Multiplication  
+# Seeking input
 
-
+print ("Write two numbers which are integers")
+number_1 = int(input ("enter your first number :"))
+number_2 = int(input ("enter yuur second number :"))
 prod = number_1 *  number_2
 print("The product of your numbers are:" ,prod)
 
