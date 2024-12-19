3bbbb1c3783832140c2c0f534400ac7664c7f9d6
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-19 17:39:37 +0530

    Update learned.py

--- learned.py
+++ learned.py
@@ -1,11 +1,8 @@
-# Addition  , subtraction , multiplication
-
+# Addition  
+# Seeking input
 print ("Write two numbers which are integers")
-
 number_1 = int(input ("enter your first number :"))
-
 number_2 = int(input ("enter yuur second number :"))
-
 sum = number_1 + number_2
 
 diff = number_1 - number_2
