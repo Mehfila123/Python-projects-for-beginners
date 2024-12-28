7c1bea89528a969a06cb7cc4fd9c254c4550a199
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-28 17:25:45 +0530

    Update Addition.py

--- Basics.py/Addition.py
+++ Basics.py/Addition.py
@@ -1,9 +1,15 @@
 # Addition  
 # Seeking input
 
+#print any 2 numbers.
+
 print ("Write two numbers which are integers")
+
+#let the numbers be number1 and number2 .
 number_1 = int(input ("enter your first number :"))
 number_2 = int(input ("enter yuur second number :"))
+
+#declare variable sum.
 sum = number_1 + number_2
 print("The sum of your numbers are:" , sum)
 
