80a3f125055da60254a968035cb72ab169a8d8c0
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-28 17:28:02 +0530

    Update Subtaction.py

--- Basics.py/Subtaction.py
+++ Basics.py/Subtaction.py
@@ -1,7 +1,11 @@
 # Subraction 
 # Seeking input
 
+#print any 2 numbers
+
 print ("Write two numbers which are integers")
+
+#recieve number1 and number2 as integer
 number_1 = int(input ("enter your first number :"))
 number_2 = int(input ("enter yuur second number :"))
 diff = number_1 - number_2
